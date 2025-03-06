3326d3c6e437ea264675b14452dc17fb6ff7cdd0
Sadiki Benda <Sadiki.benda1@gmail.com>
2025-03-06 10:55:33 -0500

    width update

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 
 // code below is not working at moment
 
-document.getElementById('payWithCashApp').addEventListener('click', function() {
-    const amount = document.getElementById('amount').value;
-    const recipient = "$SadikiBenda"; // Example: $YourBusinessTag
+let button = document.getElementById("payWithCashApp");
 
-    if (amount) {
-        window.location.href = `https://cash.app/$SadikiBneda/${amount}`;
-    } else {
-        alert("Please enter an amount before proceeding.");
-    }
-});
+button.onclick = function() {
+    console.log("Button is clicked");
+}
+
+
+
